660d4fa423c40ea7867fbde572a51858504e1369
Paul Osborne <osbpau@gmail.com>
2013-03-02 01:25:53 -0600

    Significant progress toward the first assignment, just need button functionality

--- assignment1-motorspin/motor.c
+++ assignment1-motorspin/motor.c
@@ -1,39 +1,115 @@
+/*
+ * 
+ */
 #include <pololu/orangutan.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define MOTOR_SPEED                  (100)
+#define NUMBER_DARK_REGIONS          (32)
+#define NUMBER_TRANSTIONS_ROTATION   (NUMBER_DARK_REGIONS * 2)
+
+typedef enum {
+  DIRECTION_REVERSE = -1,
+  DIRECTION_FORWARD = 1
+} direction_t;
+
+typedef struct {
+  /* the last d0 reading */
+  uint8_t d0_value;
+  /* the last d1 reading */
+  uint8_t d1_value;
+  /* the number of transitions (1 -> 0 or 0 -> 1) */
+  uint8_t number_transitions;
+  /* direction (forward/revrese) for the motor */
+  direction_t direction;
+} motor_state_t;
+
+/* Globals */
+static motor_state_t g_motor_state;
+static int g_tick;
 
 /*
- * motors1: for the Orangutan LV, SV, SVP, X2, Baby-O and 3pi robot.
- *
- * This example uses the OrangutanMotors functions to drive
- * motors in response to the position of user trimmer potentiometer
- * and blinks the red user LED at a rate determined by the trimmer
- * potentiometer position.  It uses the OrangutanAnalog library to measure
- * the trimpot position, and it uses the OrangutanLEDs library to provide
- * limited feedback with the red user LED.
- *
- * http://www.pololu.com/docs/0J20
- * http://www.pololu.com
- * http://forum.pololu.com
+ * MOTOR FUNCTIONS
  */
+static void motor_init()
+{
+  /* setup pins as inputs */
+  set_digital_input(IO_D0, PULL_UP_ENABLED);
+  set_digital_input(IO_D1, PULL_UP_ENABLED);
 
+  /* initial data reading */
+  g_motor_state.number_transitions = 0;
+  g_motor_state.d0_value = is_digital_input_high(IO_D0) ? 1 : 0;
+  g_motor_state.d1_value = is_digital_input_high(IO_D1) ? 1 : 0;
+  g_motor_state.direction = DIRECTION_FORWARD;
+}
+
+static void motor_readState()
+{
+  uint8_t new_d0_value, new_d1_value;
+  new_d0_value = is_digital_input_high(IO_D0) ? 1 : 0;
+  new_d1_value = is_digital_input_high(IO_D1) ? 1 : 0;
+  
+  /* check for transitions... for now just look at D0 */
+  if (new_d0_value != g_motor_state.d0_value) {
+    if (++g_motor_state.number_transitions > (2 * NUMBER_TRANSTIONS_ROTATION)) {
+      g_motor_state.number_transitions = 0;
+      g_motor_state.direction = g_motor_state.direction * -1; // invert
+    }
+  }
+  /* record current state */
+  g_motor_state.d0_value = new_d0_value;
+  g_motor_state.d1_value = new_d1_value;
+}
+
+static void motor_drive() {
+  set_motors(0, g_motor_state.direction * MOTOR_SPEED);  
+}
+
+/*
+ * BUTTON FUNCTIONS
+ */
+static void button_init() {
+}
+
+static void button_readState() {
+}
+
+
+/*
+ * PRINT FUNCTIONS
+ */
+static void print_service() {
+  char buf1[128];
+  char buf2[128];
+  if (g_tick % 1000 == 0) {
+    clear();
+    sprintf(buf1, "D0: %u, D1: %u",
+	    g_motor_state.d0_value,
+	    g_motor_state.d1_value);
+    print(buf1);
+    lcd_goto_xy(0, 1);  // second row
+    sprintf(buf2, "Count: %u",
+	    g_motor_state.number_transitions);
+    print(buf2);
+  }
+}
+
+/*
+ * Main Loop
+ */
 int main()
 {
-  int i = -255;
-  int going_up = 1;
+  motor_init();
+  button_init();
+  g_tick = 0;
   while(1)
   {
-    if (going_up) {
-      if (++i > 255) {
-        going_up = 0;
-        i = 255;
-      }
-    } else {
-      if (--i < -255) {
-        going_up = 1;
-        i = -255;
-      }
-    }
-    set_motors(0, i);
-    //set_motors(0, 0);
-	delay_ms(100);
+    g_tick++; /* note: will roll over */
+    motor_readState();
+    motor_drive();
+    button_readState();
+    print_service();
   }
 }
