4977201279f45cd865199acde9ebcfdd52599034
Paul Osborne <osbpau@gmail.com>
2013-03-18 00:26:40 -0500

    lab1: reformat a few things in the menu

--- lab1/menu.c
+++ lab1/menu.c
@@ -1,100 +1,101 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "menu.h"
-#include "usart.h"
 #include "leds.h"
 #include "timers.h"
+#include "log.h"
 
-void menu_init() {
-	// nothing to do really.
-	printf("MENU: {T/P/Z} {R/G/Y/A} <int>\n");
-	printf("> ");
+void menu_init()
+{
+    // nothing to do really.
+    printf("MENU: {T/P/Z} {R/G/Y/A} <int>\n");
+    printf("> ");
 }
 
 // Parse user-input and implement commands
-void menu() {
+void menu(led_state_t * led_state, timers_state_t * timers_state)
+{
+    char color;
+    char op_char;
+    uint8_t* input;
+    int parsed;
+    int value;
 
-	char color;
-	char op_char;
-	uint8_t* input;
-	int parsed;
-	int value;
+    input = USART1_get_input();
+    parsed = sscanf(input, "%c %c %d", &op_char, &color, &value);
+    printf("Parsed as op:%c color:%c value:%d\n", op_char, color, value);
 
-	input = USART1_get_input();
-	parsed = sscanf(input, "%c %c %d", &op_char, &color, &value);
-	printf("Parsed as op:%c color:%c value:%d\n", op_char, color, value);
+    /* convert color to upper and check if valid*/
+    color -= 32 * (color >= 'a' && color <= 'z');
+    switch (color) {
+    case 'R':
+    case 'G':
+    case 'Y':
+    case 'A':
+        break;
+    default:
+        printf("Bad color\n");
+        return;
+    }
 
-	/* convert color to upper and check if valid*/
-	color -= 32 * (color >= 'a' && color <= 'z');
-	switch (color) {
-	case 'R':
-	case 'G':
-	case 'Y':
-	case 'A':
-		break;
-	default:
-		printf("Bad color\n");
-		return;
-	}
+    /* Check valid command and implement */
+    switch (op_char) {
 
-	/* Check valid command and implement */
-	switch (op_char) {
+    /* change toggle frequencey for <color> LED */
+    case 'T':
+    case 't':
+        set_toggle(color, value);
+        break;
 
-	/* change toggle frequencey for <color> LED */
-	case 'T':
-	case 't':
-		set_toggle(color, value);
-		break;
+        /* print counter for <color> LED */
+    case 'P':
+    case 'p':
+        switch (color) {
+        case 'R':
+            printf("R toggles: %d\n", G_red_toggles);
+            break;
+        case 'G':
+            printf("G toggles: %d\n", G_green_toggles);
+            break;
+        case 'Y':
+            printf("Y toggles: %d\n", G_yellow_toggles);
+            break;
+        case 'A':
+            printf(
+                    "toggles R:%d  G:%d  Y:%d\n", (int) G_red_toggles, (int) G_green_toggles, (int) G_yellow_toggles);
+            break;
+        default:
+            printf("How did you get here too??\n");
+            break;
+        }
+        break;
 
-		/* print counter for <color> LED */
-	case 'P':
-	case 'p':
-		switch (color) {
-		case 'R':
-			printf("R toggles: %d\n", G_red_toggles);
-			break;
-		case 'G':
-			printf("G toggles: %d\n", G_green_toggles);
-			break;
-		case 'Y':
-			printf("Y toggles: %d\n", G_yellow_toggles);
-			break;
-		case 'A':
-			printf("toggles R:%d  G:%d  Y:%d\n", (int) G_red_toggles,
-					(int) G_green_toggles, (int) G_yellow_toggles);
-			break;
-		default:
-			printf("How did you get here too??\n");
-			break;
-		}
-		break;
+        /* zero counter for <color> LED */
+    case 'Z':
+    case 'z':
+        switch (color) {
+        case 'R':
+            G_red_toggles = 0;
+            break;
+        case 'G':
+            G_green_toggles = 0;
+            break;
+        case 'Y':
+            G_yellow_toggles = 0;
+            break;
+        case 'A':
+            G_red_toggles = G_green_toggles = G_yellow_toggles = 0;
+            break;
+        default:
+            printf("How did you get here 3??\n");
+            break;
+        }
+        break;
+    default:
+        printf("%s does not compute??\n", input);
+        break;
 
-		/* zero counter for <color> LED */
-	case 'Z':
-	case 'z':
-		switch (color) {
-		case 'R':
-			G_red_toggles = 0;
-			break;
-		case 'G':
-			G_green_toggles = 0;
-			break;
-		case 'Y':
-			G_yellow_toggles = 0;
-			break;
-		case 'A':
-			G_red_toggles = G_green_toggles = G_yellow_toggles = 0;
-			break;
-		default:
-			printf("How did you get here 3??\n");
-			break;
-		}
-		break;
-	default:
-		printf("%s does not compute??\n", input);
-		break;
-
-	} // end switch(op_char)
-	printf("> ");
+    } // end switch(op_char)
+    printf("> ");
 } // end menu()
 
