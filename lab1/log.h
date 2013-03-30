b5a737dcfcbba2d62b2b1cb23ec1f20440279e38
Paul Osborne <osbpau@gmail.com>
2013-03-30 18:26:36 -0500

    logging: make logging simpler by default (bugs without other impl)

--- lab1/log.h
+++ lab1/log.h
@@ -2,13 +2,17 @@
 #define _LOG_H
 #include <stdint.h>
 
-#ifndef LOG_DISABLE_LOGGING
-#define LOG(lvl, fmt, args...) (log_message(lvl, fmt, ##args))
-#else
-#define LOG(lvl, msg) ()
-#endif
-#define printf(args...) (LOG(LVL_DEBUG, ##args))
 
+typedef enum {
+  LVL_DEBUG = 0,
+  LVL_INFO = 1,
+  LVL_WARN = 2,
+  LVL_ERROR = 3,
+  LVL_CRITICAL = 4,
+  LVL_PRINTF = 5
+} log_level_e;
+
+#define LOG(lvl, fmt, args...) (log_message(lvl, fmt, ##args))
 #ifndef LOG_BUFFER_SIZE
 #define LOG_BUFFER_SIZE (256)
 #endif
@@ -17,15 +21,6 @@ typedef struct {
   char serbuf[LOG_BUFFER_SIZE];
   int bytes_buffered;
 } log_serial_state_t;
-
-typedef enum {
-  LVL_DEBUG = 0,
-  LVL_INFO = 1,
-  LVL_WARN = 2,
-  LVL_ERROR = 3,
-  LVL_CRITICAL = 4
-} log_level_e;
-
 void log_init();
 void log_service();
 void log_message(log_level_e lvl, char *fmt, ...);
